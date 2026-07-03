00f6c2c3df1c623298e0380562ca4e0a18a02c5b
kangsernwork <lowks-wm22@student.tarc.edu.my>
2026-07-03 10:00:00 +0800

    JOB-01

---
+++ src/util/PasswordUtil.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.security.MessageDigest;
+
+public class PasswordUtil {
+
+    public static String encrypt(String password) {
+
+        try {
+
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+            byte[] hash = md.digest(password.getBytes());
+
+            StringBuilder sb = new StringBuilder();
+
+            for (byte b : hash) {
+
+                sb.append(String.format("%02x", b));
+
+            }
+
+            return sb.toString();
+
+        } catch (Exception e) {
+
+            return password;
+
+        }
+
+    }
+
+}
